b86c2a3dc488fa68e46d45d2639f9b1467befe6e
vikramChaurasiya <bhartivikram633@gmail.com>
2025-03-24 09:38:04 +0530

    create schema and user models

--- src/app.js
+++ src/app.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const app = express();
 const { dbConnection } = require('./config/database')
-
 const PORT = process.env.PORT || 8000;
-dbConnection().then(()=>{
+const User = require("./models/user")
+
+app.post("/signup", async(req, res) => {
+  // creating a new instance of the user models 
+  const user = new User({
+    firstName: "sachin",
+    lastName: "tendulkar",
+    emailId: "sachin@tendulkar.com",
+    password: "sachin@1234"
+  });
+
+ try {
+  await user.save();
+  res.send("User added successfully");
+ } catch (err) {
+  res.status(400).send("error saving the user:",err.message)
+ }
+
+})
+
+
+
+dbConnection()
+.then(()=>{
   console.log('Database is successfully connected!')
   app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
   })
-}).catch((err)=>{
+})
+.catch((err)=>{
   console.log("databased not connect",err);
     
 })
