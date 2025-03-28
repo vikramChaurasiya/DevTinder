243ae114f5306dae6a6e128c0ce4b08ab79d535d
vikramChaurasiya <bhartivikram633@gmail.com>
2025-03-29 02:07:08 +0530

    update database Api file

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName:{
-        type: String
+        type: String,
+        required:true,
     },
     lastName:{
         type: String
     },
     emailId:{
-        type: String
+        type: String,
+        required:true,
+    },
+    passward:{
+        type: String,
+        required:true
     },
     age:{
         type: Number
