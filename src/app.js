c20286559c47be0ae21902bfabcb51004c329d97
vikramChaurasiya <bhartivikram633@gmail.com>
2025-04-01 01:54:46 +0530

    add Api level validation

--- src/app.js
+++ src/app.js
@@ -57,21 +57,40 @@ app.delete("/user" , async(req,res) =>{
 })
 
 //update date from database.
-app.patch("/user", async(req,res)=>{
-  const userId = req.body.userId;
+app.patch("/user/:userId", async(req,res)=>{
+  const userId = req.params?.userId;
   const data = req.body;
+
   try {
-    await User.findByIdAndUpdate({_id:userId}, data);
+    const ALLOWED_UPDATES = [
+      "photoUrl",
+      "about",
+      "gender",
+      "age",
+      "skills",
+    ];
+    const isUpdateAllowed = Object.keys(data).every((k)=>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if(!isUpdateAllowed){
+      throw new Error("Update not allowed")
+    }
+    if(data?.skills.length > 10){
+      throw new Error("Skills cannot more than 10 ")
+    }
+    const user = await User.findByIdAndUpdate({_id:userId}, data ,{
+      returnDocument:'after',
+      returnValidators:true,
+    });
+    console.log(user);
     res.send("User is successfully updated")
   } catch (err) {
-    res.status(400).send("Something went wrong ")
+    res.status(400).send("UPDATE FAILED "+err.message)
   }
 })
 
-app.patch("/users", async (req,res)=>{
+app.patch("/users  ", async (req,res)=>{
   const userEmail = req.body.emailId;
-  console.log(userEmail);
-  
   const data = req.body;
   
   try {
