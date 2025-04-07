5453dcc2cfb7c1df643830f63eb940252f386118
vikramChaurasiya <bhartivikram633@gmail.com>
2025-04-07 21:25:27 +0530

    create a tokrn validation

---
+++ src/middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const User = require("../models/user");
+
+const userAuth = async (req , res , next) => {
+   
+    try {
+        const {token} = req.cookies;
+        if(!token){
+            throw new Error("Token is not valid!!!!")
+        }
+        const decodedObj = await jwt.verify(token , "DEV@Tinder$754");
+        const {_id} = decodedObj;
+        const user = await User.findById(_id);
+        if(!user){
+            throw new Error("user not found ")
+        }
+        req.user = user;
+        next();
+    } catch (err) {
+        res.status(404).send("ERROR" + err.message)
+    }
+
+}
+
+module.exports = {
+    userAuth,
+}
