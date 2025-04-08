13cae588eee5977102207ec878ae7244a2fa716e
vikramChaurasiya <bhartivikram633@gmail.com>
2025-04-09 02:00:00 +0530

    seprate some code

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const userSchema = new mongoose.Schema(
     {
         firstName:{
@@ -65,5 +68,20 @@ const userSchema = new mongoose.Schema(
         timestamps: true, //this is use for when is create , update timing show
     }
 ); 
+
+userSchema.methods.getJWT = async function(){
+    const user = this;
+    const tokenPassword = "DEV@Tinder$754";
+    const token = await jwt.sign({_id: user._id}, tokenPassword , {expiresIn: "7d"});
+    return token;
+};
+
+userSchema.methods.validatePassword = async function(passwordInputByUser) {
+    const user =  this;
+    const passwordHash = user.password;
+
+    const isPasswordValid = await bcrypt.compare(passwordInputByUser , passwordHash);
+    return isPasswordValid;
+}
 
 module.exports = mongoose.model("User" , userSchema);
