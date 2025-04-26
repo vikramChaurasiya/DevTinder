8b707fb134819ee0f923111773b5f9c532b20e3b
vikramChaurasiya <bhartivikram633@gmail.com>
2025-04-26 15:07:07 +0530

    create a request review API with proper validation

--- src/routes/request.js
+++ src/routes/request.js
@@ -52,4 +52,37 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth , async (req,res)
     }
 })
 
+
+requestRouter.post("/request/review/:status/:requestId", userAuth, async(req, res)=>{
+    try {
+        const loggedInUser = req.user;
+        const {status, requestId} = req.params;
+        const allowedStatus = ["accepted", "rejected"];
+        // validate the status
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({ message : "Status not allowed!"})
+        }
+
+        // requestId find in database call 
+        const connectionRequest = await ConnectionRequest.findOne({
+            _id: requestId,
+            toUserId:loggedInUser._id,
+            status: "interested",
+        })
+        if(!connectionRequest){
+            return res
+                .status(400)
+                .json({ message: "Connection request is not found"});
+        }
+        
+        connectionRequest.status = status;
+        const data = await connectionRequest.save();
+        res.json({message: "Connection request " + status, data})
+
+
+    } catch (err) {
+        res.status(400).send("ERROR : "+err.message)
+    }
+})
+
 module.exports = requestRouter;
