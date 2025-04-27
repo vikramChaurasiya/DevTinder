57852176f85c515f50d1fc420b3be83a381aa298
vikramChaurasiya <bhartivikram633@gmail.com>
2025-04-27 14:49:26 +0530

    create a received connection request API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const userRouter= express.Router();
-const {userAuth} = require("../middlewares/auth")
+const {userAuth} = require("../middlewares/auth");
+const ConnectionRequest = require('../models/connectionRequest');
 
-userRouter.get("/user/request", userAuth,async (req, res)=>{
+// get all the pending connnection request for the loggedIn user
+userRouter.get("/user/request/received", userAuth,async (req, res)=>{
     try {
         const loggedInUser = req.user
+
+        const connectionRequests = await ConnectionRequest.find({
+            toUserId: loggedInUser._id,
+            status: "interested",
+        }).populate(
+            "fromUserId", 
+            "firstName lastName photoUrl age gender about skills"
+        );
+        // }).populate("fromUserId", ["firstName", "lastName"]);
+
+
+        res.json({
+            message: "Data fetch successfully",
+            data:connectionRequests
+        })
         
     } catch (err) {
         req.statusCode(400).send("ERROR " + err.message)
