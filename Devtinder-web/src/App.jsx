3842d191bdcf42695a87cd7c1ed1aa74e292e658
vikramChaurasiya <bhartivikram633@gmail.com>
2025-05-04 13:52:19 +0530

    add provider

--- Devtinder-web/src/App.jsx
+++ Devtinder-web/src/App.jsx
@@ -2,20 +2,23 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Body from "./Body";
 import Login from "./Login";
 import Profile from "./Profile";
+import { Provider } from "react-redux"
+import appStore from "./utils/appStore";
 
 function App() {
-  
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body/>}>
-            <Route path="/login" element={<Login/>} />
-            <Route path="/Profile" element={<Profile/>} />
-          </Route>
-        </Routes>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body/>}>
+              <Route path="/login" element={<Login/>} />
+              <Route path="/Profile" element={<Profile/>} />
+            </Route>
+          </Routes>
 
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
