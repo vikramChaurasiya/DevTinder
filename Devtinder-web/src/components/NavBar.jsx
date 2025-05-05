e56f0a3fcb357dcc26fb9acaae134b8bd8dafb22
vikramChaurasiya <bhartivikram633@gmail.com>
2025-05-05 09:28:40 +0530

    Add link tage

--- Devtinder-web/src/components/NavBar.jsx
+++ Devtinder-web/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   const user = useSelector((store) => store.user)
@@ -7,7 +8,7 @@ const NavBar = () => {
     <>
         <div className="navbar bg-base-300 shadow-sm">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">👋IshqRaah</a>
+                <Link to="/" className="btn btn-ghost text-xl">👋IshqRaah</Link>
             </div>
             {user && <div className="flex gap-1">
                 <p className='items-center flex font-medium text-xl'>Welcome, vikram</p>
@@ -23,10 +24,10 @@ const NavBar = () => {
                     tabIndex={0}
                     className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                     <li>
-                      <a className="justify-between">
+                      <Link to="/profile" className="justify-between">
                         Profile
                         <span className="badge">New</span>
-                      </a>
+                      </Link>
                     </li>
                     <li><a>Settings</a></li>
                     <li><a>Logout</a></li>
