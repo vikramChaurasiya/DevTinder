9e9d9088455abc8c9e4351ae6ada0dd734c6d4c9
vikramChaurasiya <bhartivikram633@gmail.com>
2025-05-06 12:42:35 +0530

    bulid feed page

--- Devtinder-web/src/components/Feed.jsx
+++ Devtinder-web/src/components/Feed.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { BASE_URL } from '../utils/constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { addFeed } from '../utils/feedSlice';
+import Usercard from './Usercard';
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed)
+  
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if(feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/user/feed", {
+       withCredentials: true
+      });
+      dispatch(addFeed(res.data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    getFeed();
+  },[])
+
   return (
-    <div>
-      feed
-    </div>
+    feed && (
+    <div className='flex justify-center my-30'>
+      <Usercard user={feed[1]}/>
+    </div>)
   )
 }
 
