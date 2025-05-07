04ded86e62bba486d4ac84b82a0c5e20a4a1929e
vikramChaurasiya <bhartivikram633@gmail.com>
2025-05-07 11:57:13 +0530

    add component

--- Devtinder-web/src/components/Profile.jsx
+++ Devtinder-web/src/components/Profile.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import EditProfile from './EditProfile'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+  const user = useSelector((store)=> store.user)
   return (
-    <>
-      <h1>Profile</h1>
-    </>
-  )
-}
+     user&&(
+     <>
+       <EditProfile user = {user}/>
+       {/* <h1>profile</h1> */}
+     </>
+    )
+  );
+};
 
 export default Profile
