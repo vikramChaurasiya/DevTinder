84bcc5df78bb7c13ae572fe7ca5d8b743379a4a2
vikramChaurasiya <bhartivikram633@gmail.com>
2025-05-07 11:55:57 +0530

    add edit feature

---
+++ Devtinder-web/src/components/EditProfile.jsx
@@ -0,0 +1,149 @@
+import React, { useState } from 'react'
+import Usercard from "./Usercard"
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+
+const EditProfile = ({user}) => {
+
+    const [firstName ,setFirstName] = useState(user.firstName);
+    const [lastName ,setLastName] = useState(user.lastName);
+    const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
+    const [age ,setAge] = useState(user.age);
+    const [gender, setGender] = useState(user.gender);
+    const [about, setAbout] = useState(user.about);
+    const [error, setError] = useState("");
+    const dispatch = useDispatch();
+    const [showToast, setShowToast] = useState(false);
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+          setPhotoUrl(URL.createObjectURL(file));
+        }
+    };
+
+
+    const saveProfile = async() => {
+        setError("")
+        try {
+            const res = await axios.patch(BASE_URL + "/profile/edit", {
+                firstName,lastName,photoUrl,age,gender,about
+            },
+            {withCredentials:true}
+            );
+            dispatch(addUser(res?.data?.data));
+            setShowToast(true);
+            const i = setTimeout(()=> {
+                setShowToast(false);
+            }, 3000)
+        } catch (err) {
+            setError(err.response.data);
+        }
+    }
+
+  return (
+    <>
+        <div className='flex justify-center m-30 gap-5'>
+            <div className='flex justify-center '>
+                <div className="card bg-base-200 text-primary-content w-96">
+                    <div className="card-body">
+                        <h2 className="card-title justify-center">Edit Profile</h2>
+                        <div>
+                            <label className="input validator">
+                                <div className='label'>
+                                    <span className='label-text'>FirstName</span>
+                                </div>
+                                <input 
+                                    type='text'
+                                    value={firstName}
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label className="input validator">
+                                <div className='label'>
+                                    <span className='label-text'>LastName</span>
+                                </div>
+                                <input 
+                                    type='text'
+                                    value={lastName}
+                                    onChange={(e) => setLastName(e.target.value)}
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <input 
+                                type="file" 
+                                className="file-input" 
+                                placeholder='Edit'
+                                onChange={handleFileChange}
+                            />
+                            {/* {photoUrl && (
+                                <img src={photoUrl} alt="Selected" className="mt-4 w-32 h-32 object-cover" />
+                            // )}  if you need below photo see*/} 
+                        </div>
+                        <div>
+                            <label className="input validator">
+                                <div className='label'>
+                                    <span className='label-text'>Age</span>
+                                </div>
+                                <input 
+                                    type='text'
+                                    value={age}
+                                    onChange={(e) => setAge(e.target.value)}  
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label className="input validator">
+                                <div className='label'>
+                                    <span className='label-text'>gender</span>
+                                </div>
+                                <input 
+                                    type='text'
+                                    value={gender}
+                                    onChange={(e) => setGender(e.target.value)}  
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <div className='label'>
+                                    <span className='label-text'>About</span>
+                                </div>
+                                {/* <input 
+                                    type='text-area'
+                                    value={about}
+                                    onChange={(e) => setAbout(e.target.value)}  
+                                /> */}
+                                <textarea 
+                                    className="textarea" 
+                                    placeholder="About" 
+                                    value={about}
+                                    onChange={(e) => setAbout(e.target.value)}  
+                                >About</textarea>
+                            </label>
+                        </div>
+                        <p className='text-red-500'>{error}</p>
+                        <div className="card-actions justify-center">
+                          <button className="btn btn-outline btn-success" onClick={saveProfile} >Save Profile</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <Usercard user={{firstName,lastName,photoUrl,age,gender,about}}/>
+        </div>
+        {showToast && <div className="toast toast-top toast-center">
+            <div className="alert alert-success">
+              <span> Profile successfully edit.</span>
+            </div>
+        </div>}
+    </>
+  )
+}
+
+export default EditProfile
