31996f26d15654153fe79374faeb9c2863a0ff1d
vikramChaurasiya <bhartivikram633@gmail.com>
2025-05-08 14:59:50 +0530

    add connection

--- Devtinder-web/src/App.jsx
+++ Devtinder-web/src/App.jsx
@@ -5,6 +5,7 @@ import Profile from "./components/Profile";
 import { Provider } from "react-redux"
 import appStore from "./utils/appStore";
 import Feed from "./components/Feed";
+import Connections from "./components/Connections";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
             <Route path="/" element={<Body/>}>
               <Route path="/" element={<Feed/>} />
               <Route path="/login" element={<Login/>} />
-              <Route path="/Profile" element={<Profile/>} />
+              <Route path="/profile" element={<Profile/>} />
+              <Route path="/connections" element={<Connections/>} />
             </Route>
           </Routes>
 
