1d3ba0b9f5f894ff46e92a960273e50a79d9150f
vikramChaurasiya <bhartivikram633@gmail.com>
2025-05-13 11:27:58 +0530

    Add userCard feature accept and ignore

--- Devtinder-web/src/components/Usercard.jsx
+++ Devtinder-web/src/components/Usercard.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { BASE_URL } from '../utils/constants';
+import { removeUserFeed } from '../utils/feedSlice';
+
+
 
 const Usercard = ({user}) => {
     // console.log(user);
-    const {firstName,lastName,photoUrl,age,gender, about,skills} = user;
-    
+    const { _id, firstName,lastName, photoUrl ,age,gender, about} = user;
+    const dispatch = useDispatch();
+
+    const handelSendRequest = async(status, userId ) => {
+      try {
+        const res = await axios.post(
+          BASE_URL + "/request/send/" + status + "/" + userId,
+          {},
+          {withCredentials: true},
+        )
+        dispatch(removeUserFeed(userId));
+
+
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
   return (
     <div>
         <div className="card bg-base-200 w-96 shadow-sm">
             <figure className="px-10 pt-10">
               <img
-                src={user.photoUrl}
-                alt="Shoes"
+                src={photoUrl}
+                alt="photo"
                 className="rounded-xl" />
             </figure>
             <div className="card-body items-center text-center">
@@ -18,8 +39,8 @@ const Usercard = ({user}) => {
               {age && gender && <p>{age + " " + gender}</p>}
               <p>{about}</p>
               <div className="card-actions">
-                <button className="btn btn-primary">Ignore</button>
-                <button className="btn btn-primary">Interested</button>
+                <button className="btn btn-primary" onClick={() => handelSendRequest("ignored", _id)}>Ignore</button> 
+                <button className="btn btn-primary" onClick={() => handelSendRequest("interested" , _id)} >Interested</button>
               </div>
             </div>
         </div>
