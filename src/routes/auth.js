6347d3c98079bd82a1f31fb7aacc1f79db80af96
vikramChaurasiya <bhartivikram633@gmail.com>
2025-05-14 14:51:02 +0530

    some new feature add

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -26,8 +26,13 @@ authRouter.post("/signup", async(req, res) => {
       about
     });
   
-   await user.save();
-    res.send("User added successfully");
+   const savedUser = await user.save();
+   const token = await user.getJWT();
+          
+    res.cookie("token", token , {
+      expires: new Date(Date.now()+8*3600000)
+    });
+    res.json({ message: "User added successfully!", data : savedUser });
   } catch (err) {
     res.status(400).send("ERROR : "+ err.message)
   }
